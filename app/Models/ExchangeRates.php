f588d2379d8505ecd1a12c0255e1f2ac77113fde
Miodrag12345 <realsosijedad@gmail.com>
2026-01-08 21:28:52 +0100

    #Domaci 21 lekcija

---
+++ app/Models/ExchangeRates.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class ExchangeRates extends Model
+{
+
+    const CURRENCY_EUR="eur";
+    const CURRENCY_USD="usd";
+    const CURRENCY_RUB="rub";
+
+    const AVAILABLE_CURRENCIES=[
+        self::CURRENCY_EUR,self::CURRENCY_RUB,self::CURRENCY_USD
+
+    ];
+
+
+
+    protected $table="exchange_rates";
+    protected $fillable=['currency','value'];
+
+    public static  function getCurrencyForToday($currency)
+    {
+         return self::where('currency',$currency)
+             ->whereDate('created_at',Carbon::now())
+             ->first();
+    }
+}
